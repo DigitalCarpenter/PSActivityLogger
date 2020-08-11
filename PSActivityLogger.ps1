d71a7028cf993e0c8fd9ba65865cc995bb06ff4b
DigitalCarpenter <36674732+DigitalCarpenter@users.noreply.github.com>
2020-08-11 19:52:12 +0200

    Initial Commit

---
+++ PSActivityLogger.ps1
@@ -0,0 +1,20 @@
+﻿$path = "C:\"
+$date = get-date -format "yyyy-MM-dd"
+$file = ("Log_" + $date + ".log")
+$logfile = $path + "\" + $file
+
+function Write-Log()
+{
+  $message = Read-Host "Enter Log-Message"
+  $logdate = get-date -format "yyyy-MM-dd HH:mm:ss" 
+  $text = "["+$logdate+"] - " + $message
+  Write-Host $text
+  $text >> $logfile
+  if ($message -eq "EXIT") { break; }
+}
+
+while($true){
+    #run this function
+    Write-Log
+} 
+Write-Host "["+$logdate+"] - " + "Goodbye"
